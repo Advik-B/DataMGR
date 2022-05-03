faddd430f261568793219f55604b1fcb4478e6e2
Advik <advik.b@gmail.com>
2022-05-03 21:29:46 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -10,6 +10,12 @@ class MainWindow(QWidget):
     def init(self):
         apply_stylesheet(self, theme="dark_cyan.xml")
         self.setWindowTitle("Data Manager")
+        # The below line is required to resize the window but the side effect is that the window is not positioned correctly.
+        # self.setGeometry(100, 100, 800, 600)
+        # Instead of the above line, the below line is used to resize the window correctly without changing up its position.
+        self.resize(800, 600)
+        # The below line is used to show the window in the center of the screen. (The centering is done by the window
+        # manager automatically.)
         self.show()
 
 
