fd4de0eb7545294c723d9140f154076528afdbc2
Advik <advik.b@gmail.com>
2022-05-04 12:28:40 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QWidget, QApplication
 from qt_material import apply_stylesheet
 
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -10,9 +11,9 @@ class MainWindow(QWidget):
     def init(self):
         apply_stylesheet(self, theme="dark_cyan.xml")
         self.setWindowTitle("Data Manager")
-        # The below line is required to resize the window but the side effect is that the window is not positioned correctly.
-        # self.setGeometry(100, 100, 800, 600)
-        # Instead of the above line, the below line is used to resize the window correctly without changing up its position.
+        # The below line is required to resize the window but the side effect is that the window is not positioned
+        # correctly. self.setGeometry(100, 100, 800, 600) Instead of the above line, the below line is used to resize
+        # the window correctly without changing up its position.
         self.resize(800, 600)
         # The below line is used to show the window in the center of the screen. (The centering is done by the window
         # manager automatically.)
@@ -21,9 +22,9 @@ class MainWindow(QWidget):
 
 def main():
     app = QApplication(sys.argv)
-    UI = MainWindow()
+    MainWindow()
     sys.exit(app.exec_())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
